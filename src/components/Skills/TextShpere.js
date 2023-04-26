24780a4a6ece11dcd9cb669e7ff45ba58da6cf42
IngBryanMartin <ing.bryanmartin@gmail.com>
2023-04-26 16:55:21 -0500

    website upload

--- src/components/Skills/TextShpere.js
+++ src/components/Skills/TextShpere.js
@@ -1,52 +1,50 @@
-import React, { useEffect } from "react";
-
-import "./TextShpere.css";
-
-// Importing TagCloud package
+import React, { useEffect, useRef } from "react";
 import TagCloud from "TagCloud";
+import "./TextSphere.css";
 
-const TextShpere = () => {
-  // Animation settings for Text Cloud
+const TextSphere = () => {
+  const containerRef = useRef(null);
+
   useEffect(() => {
-    const container = '.tagcloud';
+    const container = containerRef.current;
     const texts = [
       "HTML",
-        "CSS",
-        "GIT",
-        "JavaScript",
-        "React",
-        "Java",
-        ".NET",
-        "C",
-        "PHP",
-        "SQLServer",
-        "MySQL",
-        "Oracle",
-        "StarUML",
-        "Photoshop",
-        "PostMan",
-        "Arduino",
-        "Cybersecurity",
+      "CSS",
+      "JavaScript",
+      "NodeJs",
+      "React",
+      "MongoDb",
+      "MySql",
+      "Git",
+      "MUI",
+      "Express",
+      "GitHub",
+      "AI",
     ];
-
     const options = {
-      radius: 300 ,
-      maxSpeed: 'normal',
-      initSpeed: 'normal',
+      radius: 300,
+      maxSpeed: "normal",
+      initSpeed: "normal",
       keep: true,
+      loop: true,
+      lockX: true,
+      lockY: true,
     };
 
     TagCloud(container, texts, options);
+
+    return () => {
+      TagCloud(container, [], {});
+    };
   }, []);
 
   return (
-    <>
-      <div className="text-shpere">
-        {/* span tag className must be "tagcloud"  */}
-        <span className="tagcloud"></span>
-      </div>
-    </>
+    <div className="text-sphere">
+      <span className="tagcloud" ref={containerRef}></span>
+    </div>
   );
 };
 
-export default TextShpere;
+
+
+export default TextSphere;
